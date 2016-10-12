3538bdd67be028895d87be80c1439d8b75fde823
Neel <Neel@SurfaceBook>
2016-10-12 15:43:57 -0400

    trying to fix 401 auth

--- Code/src/tree/BSTNode.java
+++ Code/src/tree/BSTNode.java
@@ -1,5 +1,16 @@
 package tree;
 
-public class BSTNode {
-
+public class BSTNode <T> {
+	T key;
+	BSTNode <T> left;
+	BSTNode <T> right;
+	
+	public BSTNode (T key, BSTNode <T> left, BSTNode <T> right)
+	{
+		this.key = key;
+		this.left = left;
+		this.right = right;
+		
+	}
+	
 }
